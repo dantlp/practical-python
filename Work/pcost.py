725cb1bb0ae1a688509fdcf4351c37ba7be01fde
Dante Lozano-Perez <dlozanoperez@mun.ca>
2020-07-16 00:21:22 -0230

    Excercise 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
-Cost = 0
-with open('portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        TC= float(row[1]) * float(row[2])
-        Cost += TC
-print('Total Cost')
-print(Cost)
+
+import csv
+def portfolio_cost(filename):
+    TC = 0.00
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                C = float(row[1])*float(row[2])
+                TC += C
+            except ValueError:
+                print('Bad row:', row)
+    return TC
+
+import sys
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = input('Enter a filename:')
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
