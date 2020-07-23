8ff5d5f8bf9ee83ad392f568868318f0f108d8e0
Dante Lozano-Perez <dlozanoperez@mun.ca>
2020-07-23 01:41:56 -0230

    3.14

--- Work/report.py
+++ Work/report.py
@@ -1,40 +1,21 @@
 # report.py
 import csv
+from fileparse import parse_csv
 
 def read_portfolio(filename):
     '''
     Read a stock portfolio file into a list of dictionaries with keys
     name, shares, and price.
     '''
-    portfolio = []
-    with open(filename) as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-
-        for row in rows:
-            record = dict(zip(headers, row))
-            stock = {
-                 'name'   : record['name'],
-                 'shares' : int(record['shares']),
-                 'price'   : float(record['price'])
-            }
-            portfolio.append(stock)
-
+    portfolio = parse_csv(filename, select['name', 'shares', 'price'], types=[str, int, float])
     return portfolio
 
 def read_prices(filename):
     '''
     Read a CSV file of price data into a dict mapping names to prices.
     '''
-    prices = {}
-    with open(filename) as f:
-        rows = csv.reader(f)
-        for row in rows:
-            try:
-                prices[row[0]] = float(row[1])
-            except IndexError:
-                pass
-
+    prices = parse_csv(filename, has_headers=False, types=[str, float])
+    prices = dict(prices)
     return prices
 
 def make_report_data(portfolio, prices):
@@ -68,3 +49,11 @@ def portfolio_report(portfolio_filename,prices_filename):
     print_report(report)
 
 portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
+
+if __name__ == "__main__":
+        import sys
+        
+        if len(sys.argv) 1=2:
+            print('Usage: python report.py <portfolio.csv> <prices.csv>')
+        else:
+            portfolio_report(sys.argv[1],sys.argv[2])
